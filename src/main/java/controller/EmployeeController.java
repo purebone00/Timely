eec86923045e36e4e0c20a5b53bec6ff258e5c46
Rina Hong <Rina Hong>
2017-02-16 15:53:59 -0800

    rina creates add employee page

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -16,8 +16,18 @@ import model.Employee;
 @SessionScoped
 public class EmployeeController implements Serializable{
     @Inject private EmployeeManager employeeManager;
+    @Inject private Employee emp;
     
-    private List<Employee> list;
+    public void setEmp(Employee emp) {
+		this.emp = emp;
+	}
+
+	public Employee getEmp() {
+		return emp;
+	}
+
+
+	private List<Employee> list;
     
     public List<Employee> getList() {
         if(list == null) {
@@ -29,6 +39,11 @@ public class EmployeeController implements Serializable{
     public Employee find(int id) {
         return employeeManager.find(id);
     }
+    public void add(){
+    	//Employee newEmp = new Employee(pwd, fnm, lnm, (short)0, null, null);
+    	employeeManager.persist(emp); 
+    }
+    
     
     private void refreshList() {
         list = employeeManager.getAll();
