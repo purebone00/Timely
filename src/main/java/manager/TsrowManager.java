d54b9a04b64f81c38e4131b119363a2e3422ba21
joeFong <joemanfong@gmail.com>
2017-02-16 15:53:51 -0800

    Data table for all Employees. Template Admin page

---
+++ src/main/java/manager/TsrowManager.java
@@ -0,0 +1,57 @@
+package manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.enterprise.context.Dependent;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import model.Tsrow;
+
+
+@Dependent
+@Stateless
+public class TsrowManager {
+    @PersistenceContext(unitName="Timely-persistence-unit") EntityManager em;
+    
+    
+    public Tsrow find(int id) {
+        return em.find(Tsrow.class, id);
+    }
+
+    public void persist(Tsrow tsrow) {
+        em.persist(tsrow);
+    }
+    
+    public void update(Tsrow tsrow) {
+        em.merge(tsrow);  
+    }
+   
+    public void merge(Tsrow tsrow) {
+        em.merge(tsrow);
+    } 
+    
+    public void remove(Tsrow tsrow) {
+        em.remove(tsrow);
+    }
+    
+    public List<Tsrow> getAll() {
+        TypedQuery<Tsrow> query = em.createQuery("select s from Tsrow s",
+                Tsrow.class); 
+        List<Tsrow> Tsrow = query.getResultList();
+        return Tsrow;
+    }
+    
+    public ArrayList<Tsrow> getAllForTable(String name) {
+        TypedQuery<Tsrow> query = em.createQuery("select s from Tsrow s where s.name like ?1",
+                Tsrow.class).setParameter(1, name); 
+        ArrayList<Tsrow> Tsrow = new ArrayList<>(query.getResultList().size());
+        Tsrow.addAll(query.getResultList());
+        return Tsrow;
+    }
+
+    
+}
