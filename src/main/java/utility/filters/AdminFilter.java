a9c08e4b021eb46e1f357ddc97afc242807aaf28
Albert <purebone00@gmail.com>
2017-04-10 06:18:30 -0700

    added and implemented filters, but there are like 2 system breaking bugs that ill sort out tomorrow with joe

---
+++ src/main/java/utility/filters/AdminFilter.java
@@ -0,0 +1,43 @@
+package utility.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AdminFilter implements Filter {
+    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+        
+        if (session != null && session.getAttribute("Admin") != null) {
+            chain.doFilter(req, res);
+        } else {
+            res.sendRedirect(req.getContextPath() + "/faces/index.xhtml");
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    // ...
+}
